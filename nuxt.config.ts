d2d051858887a27a227236a74f816a578360bb6c
ikapelari <67154618+ikapelari@users.noreply.github.com>
2025-03-04 16:43:48 +0100

    condent api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  }
 })
--- pages/test.vue
+++ pages/test.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-const { data: test } = await useAsyncData(() => queryCollection('content').path('/').first())
+const { data: page } = await useAsyncData(() => queryCollection('content').path('/test').first())
+
 
 useSeoMeta({
-    title: test.value?.title,
-    description: test.value?.description
+    title: page.value?.title,
+    description: page.value?.description
 })
 </script>
 
 <template>
-    <ContentRenderer v-if="test" :value="test" />
+    <ContentRenderer v-if="page" :value="page" />
     <div v-else>Test not found</div>
 </template>
